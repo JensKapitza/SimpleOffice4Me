331a247d7d62b18d50e51572afa055bd428de5fb
Jens Kapitza <kapitza@bluepair.de>
2021-05-19 01:28:06 +0200

    add samples blueprint and cmd line

--- app/__init__.py
+++ app/__init__.py
@@ -1,25 +1,92 @@
 #!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+#export PYTHONIOENCODING=utf8
 
 import os
 import random
 import sys
+import datetime
+import locale
 
+from logging.config import dictConfig
 from flask import Flask, send_from_directory, \
     render_template_string, render_template, \
-    request, session, redirect, abort, send_file
+    request, session, redirect, abort, send_file, \
+    g
+
+
+if str(locale.getpreferredencoding()).lower in ["utf-8", "utf8"]:
+    raise BaseException("Wrong encoding use utf8")
 
 if sys.version_info < (3,):
     raise BaseException("Wrong Python Version")
 
 script_dir = os.path.dirname(os.path.abspath(__file__))
+script_dir = os.path.join(script_dir, "..")
 
-template_dir = os.path.join(script_dir, "../templates")
-static_dir = os.path.join(script_dir, "../static")
+database_dir = os.path.join(script_dir, "database")
+template_dir = os.path.join(script_dir, "templates")
+static_dir = os.path.join(script_dir, "static")
 
+# logging to file
 
+dictConfig({
+    'version': 1,
+    'formatters': {'default': {
+        'format': '[%(asctime)s] %(levelname)s in %(module)s: %(message)s',
+    }},
+    'handlers': {'wsgi': {
+        'class': 'logging.StreamHandler',
+        'stream': 'ext://flask.logging.wsgi_errors_stream',
+        'formatter': 'default'
+    },
+        'file': {
+        'class': 'logging.handlers.RotatingFileHandler',
+        'formatter': 'default',
+        'filename': 'logconfig.log',
+        'maxBytes': 1024,
+        'backupCount': 3
+    }
+
+    },
+    'root': {
+        'level': 'ERROR', # INFO ERROR
+        'handlers': ['wsgi','file']
+    }
+})
+#see here 4mail logging
+#https://flask.palletsprojects.com/en/1.1.x/logging/
 app = Flask(__name__,template_folder=template_dir,static_folder=static_dir)
+app.config['DATABASE'] = os.path.join(database_dir, "my.sqlite")
 app.config['TEMPLATES_AUTO_RELOAD'] = True
 app.config['SECRET_KEY'] = 'web-session' + str(random.random())[2:]
+
+
+# blueprints
+
+from . import auth
+app.register_blueprint(auth.bp)
+
+from . import db
+db.init_app(app)
+
+
+
+@app.template_filter('datetime')
+def format_datetime(value, format='%Y-%m-%d'):
+    return value.strftime(format)
+
+
+@app.after_request
+def add_header(response):
+    res = False
+    app.logger.debug(f"debugging ist {app.debug}")
+    if not app.debug and "text/css" in str(response.content_type) or "application/javascript" in str(response.content_type):
+        then = datetime.datetime.now() + datetime.timedelta(minutes=5)
+        response.headers['Cache-Control'] = 'public,max-age=1000'
+        response.headers['Expires'] = then.strftime("%a, %d %b %Y %H:%M:%S GMT")
+    return response
+
 
 
 @app.route('/favicon.ico')
@@ -35,7 +102,9 @@ def download_file(dirname="", filename=""):
 @app.route('/')
 @app.route('/<myFile>', methods=['GET', 'POST'])
 @app.route('/<lang>/<myFile>', methods=['GET', 'POST'])
-def myindex(lang=None, myFile="-"):
+def index(lang=None, myFile="index.html"):
+    if not myFile.endswith('html'):
+        myFile += '.html'
     resultString = render_template(myFile, mylanguage=lang)
     return resultString
 
