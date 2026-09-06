9ba6431f95c173b368178c787eb80dfd6946d656
Jens <j.kapitza@schwarze-allianz.de>
2026-09-06 17:01:48 +0200

    Fix installed wrapper argument handling

--- packaging/simpleoffice4me-wrapper.sh
+++ packaging/simpleoffice4me-wrapper.sh
@@ -3,5 +3,6 @@ set -eu
 APP_DIR=/opt/simpleoffice4me
 VENV="$APP_DIR/.venv"
 [ -x "$VENV/bin/python" ] || { echo "SimpleOffice4Me virtualenv fehlt: $VENV" >&2; exit 1; }
+[ "$#" -gt 0 ] || set -- start
 cd "$APP_DIR"
-exec "$VENV/bin/python" -m tools.launcher "${@:-start}"
+exec "$VENV/bin/python" -m tools.launcher "$@"
