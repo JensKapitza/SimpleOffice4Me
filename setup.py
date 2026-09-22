41c01a4e49be8b1714fb64d2e88140fcc15c51ac
Jens <j.kapitza@schwarze-allianz.de>
2026-09-22 07:14:28 +0200

    build: keep legacy package metadata aligned for relay

--- setup.py
+++ setup.py
@@ -41,6 +41,7 @@ setup(
         "simpleoffice_network_gateway_runtime",
         "simpleoffice_gateway_rule_content",
         "simpleoffice_mini_control",
+        "simpleoffice_connection_relay",
         "simpleoffice_media_renderer",
         "simpleoffice_media_upnp",
         "simpleoffice_service_lifecycle",
