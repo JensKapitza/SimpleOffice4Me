74bf1fcefbbfad5d0d141583d69f36ba31bfb806
Jens <j.kapitza@schwarze-allianz.de>
2026-09-22 09:13:25 +0200

    Export BlobCatalogStorageAdapter

--- app/v2/adapters/__init__.py
+++ app/v2/adapters/__init__.py
@@ -1,6 +1,7 @@
 """Concrete V2 adapters for existing SimpleOffice4Me subsystems."""
 
 from .document_store import DocumentStoreStorageAdapter
+from .blob_catalog import BlobCatalogStorageAdapter
 from .audit import RevisionHistoryAuditAdapter
 
-__all__ = ["DocumentStoreStorageAdapter", "RevisionHistoryAuditAdapter"]
+__all__ = ["BlobCatalogStorageAdapter", "DocumentStoreStorageAdapter", "RevisionHistoryAuditAdapter"]
