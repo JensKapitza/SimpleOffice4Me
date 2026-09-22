553425d09f6a659444acc7c96a9e7b869d57f907
Jens <j.kapitza@schwarze-allianz.de>
2026-09-22 07:20:15 +0200

    build: package HTTPS CONNECT tunnel module

--- setup.py
+++ setup.py
@@ -42,6 +42,7 @@ setup(
         "simpleoffice_gateway_rule_content",
         "simpleoffice_mini_control",
         "simpleoffice_connection_relay",
+        "simpleoffice_https_connect_tunnel",
         "simpleoffice_media_renderer",
         "simpleoffice_media_upnp",
         "simpleoffice_service_lifecycle",
